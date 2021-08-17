9614664ef776cf41ae1fcef001072e1db2b35924
Jovany1543 <christian.marrero3@gmail.com>
2021-08-17 09:49:33 +0000

    fisnished work section functionality

--- src/components/SectionList/index.jsx
+++ src/components/SectionList/index.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './index.scss';
 
-const SectionList = ({ name, role, years, description, metaInfo }) => {
-  const sectionTemplate = description.map((data, index) => {
+const SectionList = ({
+  name,
+  position,
+  startDate,
+  endDate,
+  summary,
+  highlights,
+}) => {
+  const sectionTemplate = highlights.map((data, index) => {
     return <li key={index} dangerouslySetInnerHTML={{ __html: data }}></li>;
   });
 
@@ -12,12 +19,14 @@ const SectionList = ({ name, role, years, description, metaInfo }) => {
       <div className="section-list__info">
         <div>
           <div className="section-list__name">{name}</div>
-          <div className="section-list__role">{role}</div>
+          <div className="section-list__role">{position}</div>
         </div>
-        <div className="section-list__date">{years}</div>
+        <div className="section-list__date">
+          {startDate}-{endDate}
+        </div>
       </div>
+      <div className="">{summary}</div>
       <ul className="section-list__ul">{sectionTemplate}</ul>
-      {metaInfo && <div className="section-list__meta-info">{metaInfo}</div>}
     </div>
   );
 };
